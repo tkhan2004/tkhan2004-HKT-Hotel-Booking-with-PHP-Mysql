810ac42b95fd95551ffa7d3f346d49a6dac10135
Tran Hoang <trminhhoang12cb@gmail.com>
2024-11-13 21:46:01 +0700

    update contact

--- contact.php
+++ contact.php
@@ -1,109 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TJ Hotel - CONTACT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require('inc/links.php'); ?>
+  <title><?php echo $settings_r['site_title'] ?> - CONTACT</title>
+</head>
+<body class="bg-light">
 
-    <?php require('inc/link.php'); ?>
-    
-    </head>
-    <body class="bg-light">
+  <?php require('inc/header.php'); ?>
 
-    <?php require('inc/header.php'); ?>
-    
-    <div class="my-5 px-4">
-        <h2 class="fw-bold h-font text-center">CONTACT US</h2>
-        <div class="h-line bg-dark"></div>
-        <p  class="text-center mt-3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Temporibus incidunt odio quos <br>dolore commodi repudiandae
-        tenetur consequuntur et similique asperiores.
-        </p>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 mb-5 px-4">   
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">LIÊN HỆ VỚI CHÚNG TÔI</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+    Nếu bạn có bất kỳ thắc mắc nào hoặc muốn đặt phòng tại HKT Hotel, vui lòng liên hệ với chúng tôi qua các thông tin sau:<br>
+    Đội ngũ nhân viên thân thiện và chuyên nghiệp của chúng tôi luôn sẵn sàng hỗ trợ bạn 24/7, đảm bảo mang đến những trải nghiệm tốt nhất khi lưu trú tại HKT Hotel.
+    </p>
+  </div>
 
-    <div class="bg-white rounded shadow p-4 ">
-        <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/@9.779349,105.6189045,11z?hl=vi-VN"></iframe>
-            <h5>Địa Chỉ</h5>
-            <a href="https://maps.app.goo.gl/8wUyf2wkBSx1bdks5" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2"> 
-            <i class="bi bi-geo-alt-fill"></i> Địa Chỉ Khách Sạn
-            </a>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 mb-5 px-4">
 
-            <h5 class="mt-4">Call me</h5>
-            <a href="tel:0364195267" class="d-inline-block mb-2 text-decoration-none text-dark">
-    <i class="bi bi-telephone-fill"></i> 0364195267
-            </a>
-            <br>
-            <a href="0364195267" class="d-inline-block text-decoration-none text-dark">
-    <i class="bi bi-telephone-fill"></i> 0364195267</a>
-    
-            <h5 class="mt-4">Email</h5>
-<a href="https://mail.google.com/mail/u/0/#inbox" class="d-inline-block mb-2 text-decoration-none text-dark ">
-    <i class="bi bi-envelope-heart-fill"></i> Đây là Gmail
-</a>
+        <div class="bg-white rounded shadow p-4">
+          <iframe class="w-100 rounded mb-4" height="320px" src="<?php echo $contact_r['iframe'] ?>" loading="lazy"></iframe>
 
-            <h5 class="mt-4">Follow us</h5>
-<div class="d-flex">
-    <a href="#" class="d-inline-block me-3 text-decoration-none text-dark">
-        <i class="bi bi-twitter"></i> Twitter
-    </a>
-    <a href="#" class="d-inline-block me-3 text-decoration-none text-dark">
-        <i class="bi bi-facebook"></i> Facebook
-    </a>
-    <a href="#" class="d-inline-block text-decoration-none text-dark">
-        <i class="bi bi-instagram"></i> Instagram
-    </a>
-</div> 
+          <h5>Địa chỉ</h5>
+          <a href="<?php echo $contact_r['gmap'] ?>" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+            <i class="bi bi-geo-alt-fill"></i> <?php echo $contact_r['address'] ?>
+          </a>
 
-   </div>
-            </div>
-     
-    
-    <div class="col-lg-6 col-md-6  px-4">   
-    <div class="bg-white rounded shadow p-4">
-<form>
-    <h5>Send a message</h5>
+          <h5 class="mt-4">Số điện thoại của Khách sạn</h5>
+          <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
+          </a>
+          <br>
+          <?php 
+            if($contact_r['pn2']!=''){
+              echo<<<data
+                <a href="tel: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
+                  <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                </a>
+              data;
+            }
+          ?>
 
-    <div class="mt-3">
-        <label class="form-label" style="font-weight: 500;">Name</label>
-        <input type="text" class="form-control shadow-none">
-    </div>
-    <div class="mt-3">
-        <label class="form-label" style="font-weight: 500;">Email</label>
-        <input type="email" class="form-control shadow-none">
-    </div>
-    <div class="mt-3">
-        <label class="form-label" style="font-weight: 500;">Subject</label>
-        <input type="text" class="form-control shadow-none">
-    </div>
-    <div class="mt-3">
-        <label class="form-label" style="font-weight: 500;">Message</label>
-        <textarea class=" form-control shadow-none " rows="5" style="resize: none;" > </textarea>
-    </div>
-</form>
 
-    </div>
-            </div>
+          <h5 class="mt-4">Email</h5>
+          <a href="mailto: <?php echo $contact_r['email'] ?>" class="d-inline-block text-decoration-none text-dark">
+            <i class="bi bi-envelope-fill"></i> <?php echo $contact_r['email'] ?>
+          </a>
+
+          <h5 class="mt-4">Theo dõi chúng tôi</h5>
+          <?php 
+            if($contact_r['tw']!=''){
+              echo<<<data
+                <a href="$contact_r[tw]" class="d-inline-block text-dark fs-5 me-2">
+                  <i class="bi bi-twitter me-1"></i>
+                </a>
+              data;
+            }
+          ?>
+
+          <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block text-dark fs-5 me-2">
+            <i class="bi bi-facebook me-1"></i>
+          </a>
+          <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block text-dark fs-5">
+            <i class="bi bi-instagram me-1"></i>
+          </a>
         </div>
+      </div>
+      <div class="col-lg-6 col-md-6 px-4">
+        <div class="bg-white rounded shadow p-4">
+          <form method="POST">
+            <h5>Gửi tin nhắn</h5>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight: 500;">Tên</label>
+              <input name="name" required type="text" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight: 500;">Email</label>
+              <input name="email" required type="email" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight: 500;">Vấn đề</label>
+              <input name="subject" required type="text" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight: 500;">Nhập tin nhắn</label>
+              <textarea name="message" required class="form-control shadow-none" rows="5" style="resize: none;"></textarea>
+            </div>
+            <button type="submit" name="send" class="btn text-white custom-bg mt-3">GỬI</button>
+          </form>
         </div>
-            
-    
+      </div>
+    </div>
+  </div>
 
 
+  <?php
+  // Assuming the insert function is defined somewhere in the codebase
+  function insert($q, $values, $types) {
+      // Implementation of insert function
+  }
+
+  // ... previous code remains the same
+
+  if(isset($_POST['send']))
+  {
+      $frm_data = filteration($_POST);
+
+      $q = "INSERT INTO `user_queries`(`name`, `email`, `subject`, `message`) VALUES (?,?,?,?)";
+      $values = [$frm_data['name'],$frm_data['email'],$frm_data['subject'],$frm_data['message']];
+
+      $res = insert($q,$values,'ssss');
+      if($res==1){
+          alert('Thành công','Mail đã được gửi!');
+      }
+      else{
+          alert('Lỗi','Server đang bảo trì, xin hãy thử lại.');
+      }
+  }
+  ?>
 
 
+  <?php require('inc/footer.php'); ?>
 
-
-    
-
-    <?php require('inc/footer.php'); ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-
+</body>
 </html>
